4753dfec999bb23b1d9f5fa471c32b747198f7b4
Liz Tianjiao Liu <tianjiaoliu2012@gmail.com>
2019-09-26 10:23:47 -0700

    Create 206 Reverse Linked List.py

---
+++ 206 Reverse Linked List.py
@@ -0,0 +1,13 @@
+#The idea is to change next with prev, prev with current, and current with next
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev = None
+        curr = head
+
+        while curr:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+
+        return prev
