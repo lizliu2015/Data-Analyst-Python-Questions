268bacc98376b06288e208356a48015bdd1eaeff
Liz Tianjiao Liu <tianjiaoliu2012@gmail.com>
2019-09-26 10:12:20 -0700

    Update 20. Valid Parentheses.py

--- 20. Valid Parentheses.py
+++ 20. Valid Parentheses.py
@@ -8,3 +8,16 @@ class Solution:
             else:
                 if mapping[stack.pop()] != c: return False
         return stack == [0]
+###
+1.创建一个字典，把反括号存为value,正括号存为key--
+2.遍历这个字符串：
+--判断是否为value:
+如果存在
+保存value，
+如果不存在判断是否为Key，
+若为Key:
+>若直接输入反括号且stack=[] 返回false
+>若这个key与stack弹出值不相等 返回false
+不为key也不为value:
+>false
+3.遍历结束后，判断是否为空”如果直接输入[/{/( ，只循环一次就结束循环，且stack不为空 ，所以只能用stack判断“
