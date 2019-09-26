b57d43c1862ce16bf3a1b0f255a9fe50d78859b9
Liz Tianjiao Liu <tianjiaoliu2012@gmail.com>
2019-09-26 01:40:10 -0700

    Create 7. Two Sum.py

---
+++ 7. Two Sum.py
@@ -0,0 +1,8 @@
+#O(N^2) solution:
+class Solution:
+    def twoSums(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return false
