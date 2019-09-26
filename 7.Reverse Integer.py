d10554ceece8ff9ed1c8249fc7ffdc5ec61c2657
Liz Tianjiao Liu <tianjiaoliu2012@gmail.com>
2019-09-26 09:26:29 -0700

    Update 7.Reverse Integer.py

--- 7.Reverse Integer.py
+++ 7.Reverse Integer.py
@@ -1,3 +1,4 @@
+#mathmatical
 class Solution(object):
     def reverse(self, x):
         result = 0
@@ -12,3 +13,14 @@ class Solution(object):
             x /= 10
             
         return 0 if result > pow(2,31) else result * symbol
+ 
+# reverse index
+def reverse(x):
+  res = str(abs(x))
+  res = int(res[::-1])
+  
+  if x > 0:
+    return res
+  else:
+    return -res
+
