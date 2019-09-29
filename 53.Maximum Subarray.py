ab746b2bef61b58a872ba0f90196ca590ed1a301
Liz Tianjiao Liu <tianjiaoliu2012@gmail.com>
2019-09-28 23:15:34 -0700

    Update 53.Maximum Subarray.py

--- 53.Maximum Subarray.py
+++ 53.Maximum Subarray.py
@@ -1,3 +1,19 @@
+### O(N^2)
+class Solution:
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        m = float("-inf")
+        for i in range(len(nums)):
+            for j in range(i, len(nums)):
+                s = sum(nums[i:j+1])
+                if m < s:
+                    m = s
+        return m
+
+
 class Solution:
     def maxSubArray(self, nums):
         """
