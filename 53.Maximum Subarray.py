ab3926307aecd28530b49a8c28fb59d111e181dc
Liz Tianjiao Liu <tianjiaoliu2012@gmail.com>
2019-09-28 23:45:50 -0700

    Update 53.Maximum Subarray.py

--- 53.Maximum Subarray.py
+++ 53.Maximum Subarray.py
@@ -13,7 +13,7 @@ class Solution:
                     m = s
         return m
 
-
+###O(N)
 class Solution:
     def maxSubArray(self, nums):
         """
@@ -25,3 +25,6 @@ class Solution:
             if dp[i-1] > 0:
                 dp[i] = dp[i-1] + nums[i]
         return max(dp)
+#If the sum of a subarray is positive, it has possible to make the next value bigger, so we keep do it until it turn to negative.
+#If the sum is negative, it has no use to the next element, so we break.
+#it is a game of sum, not the elements.
