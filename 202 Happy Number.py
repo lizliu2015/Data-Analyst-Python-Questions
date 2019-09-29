5c757d0f024f6e2fc20cfffd908b5a97e4980aa8
Liz Tianjiao Liu <tianjiaoliu2012@gmail.com>
2019-09-29 15:41:18 -0700

    Update 202 Happy Number.py

--- 202 Happy Number.py
+++ 202 Happy Number.py
@@ -4,3 +4,17 @@ def isHappy(self, n):
             seen.add(n)
             n = sum([int(x) **2 for x in str(n)])
         return n == 1
+
+class Solution:
+    def isHappy(self, n):
+        s = set()
+        while n != 1:
+            sum = 0
+            for i in str(n):
+                sum = int(i)**2 + sum 
+            n = sum 
+            if n in s:
+                return False
+            else:
+                s.add(n) 
+        return True
