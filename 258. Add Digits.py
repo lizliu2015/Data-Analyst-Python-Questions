8660a3f17d35f2571a512c6e5389a735eff91369
Liz Tianjiao Liu <tianjiaoliu2012@gmail.com>
2019-10-02 19:53:33 -0700

    Create 258. Add Digits.py

---
+++ 258. Add Digits.py
@@ -0,0 +1,9 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        n = 0
+        while len(str(num)) > 1:
+            for i in str(num):
+                n = n + int(i)
+            num = n
+            n = 0
+        return num
