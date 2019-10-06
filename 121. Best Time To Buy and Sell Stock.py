b4ca256f33e028a4828c424e62a7197d1683518c
Liz Tianjiao Liu <tianjiaoliu2012@gmail.com>
2019-10-06 10:45:12 -0700

    Update 121. Best Time To Buy and Sell Stock.py

--- 121. Best Time To Buy and Sell Stock.py
+++ 121. Best Time To Buy and Sell Stock.py
@@ -10,3 +10,26 @@ class Solution(object):
                 if prices[j] - prices[i] > m:
                     m = prices[j] - prices[i]
         return m
+  
+#dynamic programming
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        diff = []
+        for i in range(1, len(prices)):
+            diff.append(prices[i] - prices[i-1])
+        
+        curr_sum = 0
+        curr_max = float('-inf')
+        
+        for j in diff:
+            if curr_sum > 0:
+                curr_sum = curr_sum + j
+            else:
+                curr_sum = j
+            if curr_max < curr_sum:
+                curr_max = curr_sum
+        
+        if curr_max > 0:
+            return curr_max
+        return 0    
+                
