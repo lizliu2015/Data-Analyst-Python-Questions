456170f3353b07c499edaa360d120e3b3b5ee3d0
Liz Tianjiao Liu <tianjiaoliu2012@gmail.com>
2019-10-06 10:27:25 -0700

    Update 53.Maximum Subarray.py

--- 53.Maximum Subarray.py
+++ 53.Maximum Subarray.py
@@ -15,16 +15,14 @@ class Solution:
 
 ###O(N)
 class Solution:
-    def maxSubArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        dp = nums.copy()
-        for i in range(1, len(nums)):
-            if dp[i-1] > 0:
-                dp[i] = dp[i-1] + nums[i]
-        return max(dp)
-#If the sum of a subarray is positive, it has possible to make the next value bigger, so we keep do it until it turn to negative.
-#If the sum is negative, it has no use to the next element, so we break.
-#it is a game of sum, not the elements.
+    def maxSubArray(self, nums: List[int]) -> int:
+        CurrMax = float('-inf')
+        CurrSum = 0
+        for i in range(len(nums)):
+            if CurrSum > 0:
+                CurrSum = nums[i] + CurrSum
+            else:
+                CurrSum = nums[i]
+            if CurrSum > CurrMax:
+                CurrMax = CurrSum
+        return CurrMax
