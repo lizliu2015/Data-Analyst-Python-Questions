f4b4263216a124bd358274b6191557e29718084c
Liz Tianjiao Liu <tianjiaoliu2012@gmail.com>
2019-10-09 00:40:22 -0700

    Update 258. Add Digits.py

--- 258. Add Digits.py
+++ 258. Add Digits.py
@@ -1,9 +1,8 @@
 class Solution:
     def addDigits(self, num: int) -> int:
-        n = 0
         while len(str(num)) > 1:
+        n = 0
             for i in str(num):
                 n = n + int(i)
             num = n
-            n = 0  #reset n
         return num
